b332cf3e80c94c5df6193e4b81adc8cc388b5fdd
Taketo Sano <taketo1024@gmail.com>
2022-12-06 12:46:07 +0900

    add CsMat::csc_from_vec

--- src/matrix/sparse.rs
+++ src/matrix/sparse.rs
@@ -1,7 +1,7 @@
-use std::ops::Deref;
+use std::ops::{Deref, Add};
 
 use num_traits::Zero;
-use sprs::{CsMatBase, SpIndex};
+use sprs::{CsMatBase, SpIndex, TriMat, CsMat};
 
 pub trait CsMatElem: Clone + Default + Send + Sync + sprs::MulAcc {}
 
@@ -10,18 +10,36 @@ impl<T> CsMatElem for T
 {}
 
 pub trait CsMatExt { 
+    type N;
+    fn csc_from_vec(shape: (usize, usize), data: Vec<Self::N>) -> CsMat<Self::N>;
     fn is_zero(&self) -> bool;
 }
 
 impl<N, I, IptrStorage, IndStorage, DataStorage, Iptr> CsMatExt for CsMatBase<N, I, IptrStorage, IndStorage, DataStorage, Iptr>
 where
-    N: Zero,
+    N: Zero + Add<Output = N> + Clone,
     I: SpIndex,
     Iptr: SpIndex,
     IptrStorage: Deref<Target = [Iptr]>,
     IndStorage: Deref<Target = [I]>,
     DataStorage: Deref<Target = [N]>,
 {
+    type N = N;
+
+    fn csc_from_vec(shape: (usize, usize), data: Vec<Self::N>) -> CsMat<Self::N> {
+        let (m, n) = shape;
+        assert!(m * n >= data.len());
+
+        let mut trip = TriMat::new(shape);
+        for (k, a) in data.into_iter().enumerate() {
+            if a.is_zero() { continue }
+            let (i, j) = (k / n, k % n);
+            trip.add_triplet(i, j, a);
+        }
+        
+        trip.to_csc()
+    }
+
     fn is_zero(&self) -> bool {
         self.data().iter().all(|a| a.is_zero())
     }

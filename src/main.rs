03c163106d19e1db873b402db68cf2e356504fe5
Taketo Sano <taketo1024@gmail.com>
2022-12-20 15:05:56 +0900

    add targets.json.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,29 @@
-use yui::links::Link;
+use std::collections::BTreeMap;
+use yui::links::{Link, links::Edge};
+use yui::khovanov::homology::KhHomology;
 
 fn main() {
-    let l = Link::empty();
-    println!("{:?}", l);
+    let data = load_data().expect("");
+    let pd = data["14n19265"].clone();
+    let l = Link::from(pd);
+
+    measure(|| {
+        let h = KhHomology::<i32>::new(&l);
+        println!("{h}");
+    });
+}
+
+type Data = BTreeMap<String, Vec<[Edge; 4]>>;
+fn load_data() -> Result<Data, Box<dyn std::error::Error>> {
+    let path = "resources/targets/targets.json";
+    let json = std::fs::read_to_string(path)?;
+    let data: Data = serde_json::from_str(&json)?;
+    Ok(data)
+}
+
+fn measure<F>(proc: F) where F: Fn() -> () { 
+    let start = std::time::Instant::now();
+    proc();
+    let dur = start.elapsed();
+    println!("time: {:?}", dur);
 }

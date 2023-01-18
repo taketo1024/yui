8a3285de36abfe765bc8dcddf0a325585dc3d173
Taketo Sano <taketo1024@gmail.com>
2023-01-18 14:27:09 +0900

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,45 @@
+#![allow(dead_code)]
+
 use std::collections::BTreeMap;
+use yui::math::traits::{EucRing, EucRingOps};
 use yui::links::{Link, links::Edge};
-use yui::khovanov::homology::KhHomology;
+use yui::khovanov::invariants::ss::ss_invariant;
 
 fn main() {
-    let data = load_data().expect("");
-    let pd = data["14n19265"].clone();
-    let l = Link::from(pd);
+    let name = "3_1";
+    let l = Link::load(name).unwrap();
+    run(&l, name, 2);
+}
 
-    measure(|| {
-        let h = KhHomology::<i32>::unreduced(l);
-        println!("{h}");
+fn run_all() { 
+    let data = load_data().expect("");
+    for (name, pd) in data.into_iter() { 
+        let l = Link::from(pd);
+        for c in [2 as i64, 3] { 
+            run(&l, &name, c)
+        }
+    }
+}
+
+fn run<R>(l: &Link, name: &str, c: R) 
+where R: EucRing, for<'x> &'x R: EucRingOps<R> { 
+    let ((h, s), time) = measure(|| {
+        ss_invariant(l, c.clone(), true)
     });
+
+    println!("l = {name}, c = {c}");
+    println!("h = {h}");
+    println!("s = {s}");
+
+    println!("\ntime: {:?}\n", time);
+}
+
+fn measure<F, Res>(proc: F) -> (Res, std::time::Duration) 
+where F: FnOnce() -> Res { 
+    let start = std::time::Instant::now();
+    let res = proc();
+    let time = start.elapsed();
+    (res, time)
 }
 
 type Data = BTreeMap<String, Vec<[Edge; 4]>>;
@@ -21,9 +50,3 @@ fn load_data() -> Result<Data, Box<dyn std::error::Error>> {
     Ok(data)
 }
 
-fn measure<F>(proc: F) where F: FnOnce() -> () { 
-    let start = std::time::Instant::now();
-    proc();
-    let dur = start.elapsed();
-    println!("time: {:?}", dur);
-}

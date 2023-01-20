20f67d0f8591efcd70d3b48a17ee35de149fbd33
Taketo Sano <taketo1024@gmail.com>
2023-01-20 10:15:55 +0900

    multi-thread version of inv_upper_tri.

--- src/math/matrix/triang.rs
+++ src/math/matrix/triang.rs
@@ -1,10 +1,25 @@
+use std::cell::RefCell;
 use std::mem::replace;
+use std::ops::DerefMut;
+use std::sync::Arc;
 use itertools::Itertools;
 use sprs::{CsMat, CsVec};
+use rayon::prelude::*;
+use thread_local::ThreadLocal;
 use crate::math::traits::{Ring, RingOps};
 use crate::math::matrix::sparse::CsVecExt;
 
 pub fn inv_upper_tri<R>(u: &CsMat<R>) -> CsMat<R>
+where R: Ring, for<'x> &'x R: RingOps<R> {
+    const MULTI_THREAD: bool = false;
+    if MULTI_THREAD { 
+        inv_upper_tri_m(u)
+    } else { 
+        inv_upper_tri_s(u)
+    }
+}
+
+fn inv_upper_tri_s<R>(u: &CsMat<R>) -> CsMat<R>
 where R: Ring, for<'x> &'x R: RingOps<R> {
     assert!(u.is_csc());
     debug_assert!(is_upper_tri(u));
@@ -37,6 +52,64 @@ where R: Ring, for<'x> &'x R: RingOps<R> {
 
         indptr.push(count);
     }
+
+    CsMat::new_csc(shape, indptr, indices, data)
+}
+
+fn inv_upper_tri_m<R>(u: &CsMat<R>) -> CsMat<R>
+where R: Ring, for<'x> &'x R: RingOps<R> {
+    assert!(u.is_csc());
+    debug_assert!(is_upper_tri(u));
+
+    let shape = u.shape();
+    let n = u.rows();
+    let diag = diag(&u);
+
+    let tls1 = Arc::new(ThreadLocal::new());
+    let tls2 = Arc::new(ThreadLocal::new());
+
+    let result: Vec<_> = (0..n).into_par_iter().map(|j| -> Vec<_> { 
+        let mut x_st = tls1.get_or(|| {
+            let x = vec![R::zero(); n];
+            RefCell::new(x)
+        }).borrow_mut();
+
+        let mut b_st = tls2.get_or(|| {
+            let b = vec![R::zero(); n];
+            RefCell::new(b)
+        }).borrow_mut();
+
+        let x = x_st.deref_mut();
+        let b = b_st.deref_mut();
+
+        b[j] = R::one();
+
+        solve_upper_tri_into(&u, &diag, b, x);
+
+        (0..=j).filter_map(|i| { 
+            if x[i].is_zero() { return None }
+            let x_i = replace(&mut x[i], R::zero());
+            Some((i, x_i))
+        }).collect()
+    }).collect();
+
+    let (indptr, indices, data) = {
+        let mut count = 0;
+        let mut indptr = vec![0];
+        let mut indices = vec![];
+        let mut data = vec![];
+    
+        for col in result { 
+            for (i, x_i) in col { 
+                count += 1;
+                indices.push(i);
+                data.push(x_i);
+            }
+            indptr.push(count);
+        }
+
+        (indptr, indices, data)
+    };
 
     CsMat::new_csc(shape, indptr, indices, data)
 }

1d2edcd2bc9b653a771c5fedc014cf44518ea42d
Taketo Sano <taketo1024@gmail.com>
2023-02-20 13:47:16 +0900

    main supports command: kh.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,15 @@ use clap::{Parser, Subcommand, ValueEnum};
 use log::{info, error};
 use simplelog::*;
 
+#[allow(non_camel_case_types)]
+#[derive(Clone, ValueEnum, Debug, derive_more::Display)]
+pub enum CType { 
+    i64, i128, bigint, 
+    gauss, gauss_128, gauss_big,
+    eisen, eisen_128, eisen_big,
+    none
+}
+
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 #[command(propagate_version = true)]
@@ -15,6 +24,25 @@ struct Cli {
 
 #[derive(Subcommand, Debug)]
 enum Commands {
+    Kh {
+        name: String,
+
+        #[arg(short, long)]
+        link: Option<String>,
+
+        #[arg(short, long, default_value = "0")]
+        c_value: String,
+
+        #[arg(short = 't', long, default_value_t = CType::i64)]
+        c_type: CType,
+
+        #[arg(long)]
+        reduced: bool,
+
+        #[arg(long)]
+        bigraded: bool,
+    },
+
     SS {
         name: String,
 
@@ -24,8 +52,8 @@ enum Commands {
         #[arg(short, long)]
         c_value: String,
 
-        #[arg(short = 't', long, default_value_t = ss::CType::i64)]
-        c_type: ss::CType,
+        #[arg(short = 't', long, default_value_t = CType::i64)]
+        c_type: CType,
 
         #[arg(short, long)]
         output: Option<String>,
@@ -35,8 +63,8 @@ enum Commands {
         targets: String,
         c_value: String,
         
-        #[arg(short = 't', long, default_value_t = ss::CType::i64)]
-        c_type: ss::CType,
+        #[arg(short = 't', long, default_value_t = CType::i64)]
+        c_type: CType,
 
         #[arg(short, long)]
         output: Option<String>,
@@ -53,6 +81,8 @@ fn main() {
     let (res, time) = measure(|| 
         guard_panic(||
             match args.command { 
+            Commands::Kh { name, link, c_value, c_type, reduced, bigraded }
+                => kh::run(name, link, c_value, c_type, reduced, bigraded),
             Commands::SS { name, link, c_value, c_type, output } 
                 => ss::run_single(name, link, c_value, c_type, output),
             Commands::SSBatch { targets, c_value, c_type, output }
@@ -73,23 +103,92 @@ fn main() {
     }
 }
 
+mod kh { 
+    use std::str::FromStr;
+    use yui::khovanov::homology::{KhHomologyBigraded, KhHomology};
+    use yui::links::Link;
+    use yui::math::homology::base::PrintTable;
+    use yui::math::traits::{EucRing, EucRingOps};
+    use super::*;
+
+    pub fn run(name: String, link: Option<String>, c_value: String, c_type: CType, reduced: bool, bigraded: bool) -> Result<String, Box<dyn std::error::Error>> {
+        let l = load_link(&name, &link)?;
+
+        if bigraded { 
+            compute_bigraded_switch(l, &c_value, &c_type, reduced)
+        } else { 
+            compute_homology_switch(l, &c_value, &c_type, reduced)
+        }
+    }
+
+    fn compute_bigraded_switch(l: Link, c_value: &String, c_type: &CType, reduced: bool) -> Result<String, Box<dyn std::error::Error>> {
+        use num_bigint::BigInt;
+        use yui::math::types::quad_int::{GaussInt, EisenInt};
+
+        match c_type { 
+            CType::i64        => compute_bigraded::<i64>(l, c_value, reduced),
+            CType::i128       => compute_bigraded::<i128>(l, c_value, reduced),
+            CType::bigint     => compute_bigraded::<BigInt>(l, c_value, reduced),
+            CType::gauss      => compute_bigraded::<GaussInt<i64>>(l, c_value, reduced), 
+            CType::gauss_128  => compute_bigraded::<GaussInt<i128>>(l, c_value, reduced), 
+            CType::gauss_big  => compute_bigraded::<GaussInt<BigInt>>(l, c_value, reduced), 
+            CType::eisen      => compute_bigraded::<EisenInt<i64>>(l, c_value, reduced), 
+            CType::eisen_128  => compute_bigraded::<EisenInt<i128>>(l, c_value, reduced), 
+            CType::eisen_big  => compute_bigraded::<EisenInt<BigInt>>(l, c_value, reduced), 
+            _ => err!("")
+        }
+    }
+
+    fn compute_bigraded<R>(l: Link, c_value: &String, reduced: bool) -> Result<String, Box<dyn std::error::Error>>
+    where R: EucRing + FromStr, for<'x> &'x R: EucRingOps<R> { 
+        if let Ok(c) = c_value.parse::<R>() { 
+            if !c.is_zero() { 
+                return err!("--bigraded only supported for `c = 0`.")
+            }
+            let h = KhHomologyBigraded::new(l, reduced);
+            let table = h.table();
+            Ok(table)
+        } else { 
+            err!("cannot parse c: '{}' as type: {}.", c_value, std::any::type_name::<R>())
+        }
+    }
+
+    fn compute_homology_switch(l: Link, c_value: &String, c_type: &CType, reduced: bool) -> Result<String, Box<dyn std::error::Error>> {
+        use num_bigint::BigInt;
+        use yui::math::types::quad_int::{GaussInt, EisenInt};
+
+        match c_type { 
+            CType::i64        => compute_homology::<i64>(l, c_value, reduced),
+            CType::i128       => compute_homology::<i128>(l, c_value, reduced),
+            CType::bigint     => compute_homology::<BigInt>(l, c_value, reduced),
+            CType::gauss      => compute_homology::<GaussInt<i64>>(l, c_value, reduced), 
+            CType::gauss_128  => compute_homology::<GaussInt<i128>>(l, c_value, reduced), 
+            CType::gauss_big  => compute_homology::<GaussInt<BigInt>>(l, c_value, reduced), 
+            CType::eisen      => compute_homology::<EisenInt<i64>>(l, c_value, reduced), 
+            CType::eisen_128  => compute_homology::<EisenInt<i128>>(l, c_value, reduced), 
+            CType::eisen_big  => compute_homology::<EisenInt<BigInt>>(l, c_value, reduced), 
+            _ => err!("")
+        }
+    }
+
+    fn compute_homology<R>(l: Link, c_value: &String, reduced: bool) -> Result<String, Box<dyn std::error::Error>>
+    where R: EucRing + FromStr, for<'x> &'x R: EucRingOps<R> { 
+        if let Ok(c) = c_value.parse::<R>() { 
+            let h = KhHomology::new(l, c, R::zero(), reduced);
+            let res = h.to_string();
+            Ok(res)
+        } else { 
+            err!("cannot parse c: '{}' as type: {}.", c_value, std::any::type_name::<R>())
+        }
+    }
+}
+
 mod ss { 
     use std::str::FromStr;
-
-    use super::*;
     use yui::links::Link;
-    use yui::khovanov::invariants::ss::ss_invariant;
-    use derive_more::Display;
     use yui::math::traits::{EucRing, EucRingOps};
-
-    #[allow(non_camel_case_types)]
-    #[derive(Clone, ValueEnum, Debug, Display)]
-    pub enum CType { 
-        i64, i128, bigint, 
-        gauss, gauss_128, gauss_big,
-        eisen, eisen_128, eisen_big,
-        none
-    }
+    use yui::khovanov::invariants::ss::ss_invariant;
+    use super::*;
 
     pub fn run_single(name: String, link: Option<String>, c_value: String, c_type: CType, output: Option<String>) -> Result<String, Box<dyn std::error::Error>> {
         let l = load_link(&name, &link)?;

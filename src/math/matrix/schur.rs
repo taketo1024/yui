b871e2d283d322319eac2e66c8e3faa389c9151f
Taketo Sano <taketo1024@gmail.com>
2023-02-28 18:25:59 +0900

    add test.

--- src/math/matrix/schur.rs
+++ src/math/matrix/schur.rs
@@ -100,103 +100,42 @@ where R: Ring, for<'x> &'x R: RingOps<R> {
     }
 }
 
-/*
 #[cfg(test)]
 mod tests { 
     use super::*;
 
     #[test]
-    fn schur_compl_block() {
-        let x = CsMat::csc_from_vec((5,5), vec![
-            3,5,1,2,3,
-            1,2,4,5,6,
-            1,0,9,8,2,
-            1,2,5,3,1,
-            3,7,3,4,7
-        ]);
-        let [a,b,c,d] = x.divide4(2,2);
-        let ainv = CsMat::csc_from_vec((2, 2), vec![2,-5,-1,3]);
-
-        let s = Schur::new(a, ainv, b, c, d);
-        assert_eq!(
-            s.complement().to_dense(), 
-            CsMat::csc_from_vec((3,3), vec![27,29,26,1,-2,-5,-20,-24,-26]).to_dense()
-        );
-    }
-
-    #[test]
-    fn trans_mats() {
-        let x = CsMat::csc_from_vec((5,5), vec![
-            3,5,1,2,3,
-            1,2,4,5,6,
-            1,0,9,8,2,
-            1,2,5,3,1,
-            3,7,3,4,7
-        ]);
-        let [a,b,c,d] = x.divide4(2,2);
-        let ainv = CsMat::csc_from_vec((2, 2), vec![2,-5,-1,3]);
-
-        let s = Schur::new(a, ainv, b, c, d);
-
-        let id = |n| CsMat::<i32>::id(n);
-        let (p, pinv) = (s.p(), s.pinv());
-        let (q, qinv) = (s.q(), s.qinv());
-
-        assert_eq!((&p * &pinv).to_dense(), id(5).to_dense());
-        assert_eq!((&q * &qinv).to_dense(), id(5).to_dense());
-
-        let y = &(&p * &x) * &q;
-        let [e,f,g,h] = y.divide4(2,2);
-
-        assert_eq!(e.to_dense(), id(2).to_dense());
-        assert!(f.is_zero());
-        assert!(g.is_zero());
-        assert_eq!(h.to_dense(), s.complement().to_dense());
-    }
-
-    #[test]
     fn schur_compl() {
-        let a = CsMat::csc_from_vec((5, 6), vec![
-            1, 2, 3, 4, 5, 6,
-            0,-1, 2, 2, 3, 2,
-            0, 0, 1, 4, 5,-3,
-            1, 2, 0,-3, 2, 1,
-            3, 2, 3, 0, 2, 8
+        let a = SpMat::from_vec((6, 5), vec![
+            1, 0, 0, 1, 3,
+            2,-1, 0, 2, 2,
+            3, 2, 1, 0, 3,
+            4, 2, 4,-3, 0,
+            5, 3, 5, 2, 2,
+            6, 2,-3, 1, 8
         ]);
-        let s = schur_partial_upper_triang(a.clone(), 3);
-
-        assert_eq!(s.complement(), CsMat::csc_from_vec((2,3), vec![5,12,-14,36,45,-60]));
-
-        let id = |n| CsMat::<i32>::id(n);
-        let (p, pinv) = (s.p(), s.pinv());
-        let (q, qinv) = (s.q(), s.qinv());
-
-        assert_eq!((&p * &pinv).to_dense(), id(5).to_dense());
-        assert_eq!((&q * &qinv).to_dense(), id(6).to_dense());
-
-        let y = &(&p * &a) * &q;
-        let [e,f,g,h] = y.divide4(3,3);
-
-        assert_eq!(e.to_dense(), id(3).to_dense());
-        assert!(f.is_zero());
-        assert!(g.is_zero());
-        assert_eq!(h.to_dense(), s.complement().to_dense());
+        let s = SchurLT::from_partial_lower(&a.view(), 3);
+        assert_eq!(s.complement(), &SpMat::from_vec((3,2), vec![5,36,12,45,-14,-60]));
     }
 
     #[test]
     fn trans_vec() {
-        let a = CsMat::csc_from_vec((5, 6), vec![
-            1, 2, 3, 4, 5, 6,
-            0,-1, 2, 2, 3, 2,
-            0, 0, 1, 4, 5,-3,
-            1, 2, 0,-3, 2, 1,
-            3, 2, 3, 0, 2, 8
+        let a = SpMat::from_vec((6, 5), vec![
+            1, 0, 0, 1, 3,
+            2,-1, 0, 2, 2,
+            3, 2, 1, 0, 3,
+            4, 2, 4,-3, 0,
+            5, 3, 5, 2, 2,
+            6, 2,-3, 1, 8
         ]);
+        let s = SchurLT::from_partial_lower(&a.view(), 3);
 
-        let s = schur_partial_upper_triang(a, 3);
-        let v = CsVec::new(5, (0..5).collect(), (0..5).collect());
+        let v = SpVec::from(vec![1,2,0,-3,2,1]);
         let w = s.trans_vec(v);
-        assert_eq!(w, CsVec::new(2, vec![0,1], vec![9, 28]));
+        assert_eq!(w, SpVec::from(vec![5,12,-14]));
+
+        let v = SpVec::from(vec![3,2,3,0,2,8]);
+        let w = s.trans_vec(v);
+        assert_eq!(w, SpVec::from(vec![36,45,-60]));
     }
 }
-*/

fa81401c79bbb2d61174d8d276de68289007cf32
Taketo Sano <taketo1024@gmail.com>
2023-03-03 17:59:19 +0900

    improve code, add vec reduction.

--- workspace/homology/src/utils/reducer2.rs
+++ workspace/homology/src/utils/reducer2.rs
@@ -1,5 +1,4 @@
 use std::collections::HashMap;
-
 use log::*;
 use sprs::PermOwned;
 use yui_matrix::sparse::*;
@@ -16,7 +15,8 @@ where
     C::Output: RModStr<R = R>
  { 
     complex: &'a C,
-    mats: HashMap<C::Idx, SpMat<R>>
+    mats: HashMap<C::Idx, SpMat<R>>,
+    vecs: HashMap<C::Idx, Vec<SpVec<R>>>
 }
 
 impl<'a, R, C> ChainReducer<'a, R, C>
@@ -27,7 +27,15 @@ where
  { 
     pub fn new(complex: &'a C) -> Self { 
         let mats = HashMap::new();
-        Self { complex, mats }
+        let vecs = HashMap::new();
+        Self { complex, mats, vecs }
+    }
+
+    pub fn set_vec(&mut self, i: C::Idx, v: SpVec<R>) {
+        if !self.vecs.contains_key(&i) { 
+            self.vecs.insert(i, vec![]);
+        }
+        self.vecs.get_mut(&i).unwrap().push(v);
     }
 
     pub fn process(&mut self) { 
@@ -46,19 +54,33 @@ where
         let a1 = self.take_matrix(i1); // target
         let a2 = self.take_matrix(i2); // next
 
-        let (b0, b1, b2) = process_triple(a0, a1, a2);
+        let v1 = self.take_vecs(i1);
+        let v2 = self.take_vecs(i2);
 
-        self.set_matrix(i0, b0);
-        self.set_matrix(i1, b1);
-        self.set_matrix(i2, b2);
+        let (a0, a1, a2, v1, v2) = process_triple(a0, a1, a2, v1, v2);
+
+        self.mats.insert(i0, a0);
+        self.mats.insert(i1, a1);
+        self.mats.insert(i2, a2);
+
+        self.vecs.insert(i1, v1);
+        self.vecs.insert(i2, v2);
+    }
+
+    pub fn matrix(&self, i: C::Idx) -> Option<&SpMat<R>> {
+        self.mats.get(&i)
     }
 
     pub fn take_matrix(&mut self, i: C::Idx) -> SpMat<R> {
         self.mats.remove(&i).unwrap_or(self.complex.d_matrix(i))
     }
 
-    fn set_matrix(&mut self, i: C::Idx, d: SpMat<R>) {
-        self.mats.insert(i, d);
+    pub fn vecs(&self, i: C::Idx) -> Option<&Vec<SpVec<R>>> {
+        self.vecs.get(&i)
+    }
+
+    pub fn take_vecs(&mut self, i: C::Idx) -> Vec<SpVec<R>> {
+        self.vecs.remove(&i).unwrap_or(vec![])
     }
 }
 
@@ -75,11 +97,11 @@ where
 //  C[0] --------> C[1]'---------> C[2]'-------> C[3]
 //          [y]           [s]           [w]
 
-fn process_triple<R>(a0: SpMat<R>, a1: SpMat<R>, a2: SpMat<R>) -> (SpMat<R>, SpMat<R>, SpMat<R>)
+fn process_triple<R>(a0: SpMat<R>, a1: SpMat<R>, a2: SpMat<R>, v1: Vec<SpVec<R>>, v2: Vec<SpVec<R>>) -> (SpMat<R>, SpMat<R>, SpMat<R>, Vec<SpVec<R>>, Vec<SpVec<R>>)
 where R: Ring, for<'x> &'x R: RingOps<R> { 
     let (p, q, r) = pivots(&a1);
     if r == 0 { 
-        return (a0, a1, a2)
+        return (a0, a1, a2, v1, v2)
     }
 
     info!("compute schur complement.");
@@ -88,11 +110,14 @@ where R: Ring, for<'x> &'x R: RingOps<R> {
 
     info!("schur complement: {:?}", s.complement().shape());
 
-    let a0 = reduce_rows(a0, &q, r);
-    let a1 = s.complement_into();
-    let a2 = reduce_cols(a2, &p, r);
+    let v1 = reduce_src_vecs(v1, &q, r);
+    let v2 = reduce_tgt_vecs(v2, &p, &s);
     
-    process_triple(a0, a1, a2)
+    let a0 = reduce_mat_rows(a0, &q, r);
+    let a1 = s.complement_into();
+    let a2 = reduce_mat_cols(a2, &p, r);
+
+    process_triple(a0, a1, a2, v1, v2)
 }
 
 fn pivots<R>(a: &SpMat<R>) -> (PermOwned, PermOwned, usize)
@@ -109,132 +134,139 @@ where R: Ring, for<'x> &'x R: RingOps<R> {
     SchurLT::from_partial_lower(b1, r)
 }
 
-fn reduce_rows<R>(a: SpMat<R>, p: &PermOwned, r: usize) -> SpMat<R>
+fn reduce_mat_rows<R>(a: SpMat<R>, p: &PermOwned, r: usize) -> SpMat<R>
 where R: Ring, for<'x> &'x R: RingOps<R> { 
     let m = a.rows();
     a.permute_rows(p.view()).submat_rows(r..m).to_owned()
 }
 
-fn reduce_cols<R>(a: SpMat<R>, q: &PermOwned, r: usize) -> SpMat<R>
+fn reduce_mat_cols<R>(a: SpMat<R>, p: &PermOwned, r: usize) -> SpMat<R>
 where R: Ring, for<'x> &'x R: RingOps<R> { 
     let n = a.cols();
-    a.permute_cols(q.view()).submat_cols(r..n).to_owned()
+    a.permute_cols(p.view()).submat_cols(r..n).to_owned()
 }
 
-#[cfg(_test)]
+fn reduce_src_vecs<R>(vs: Vec<SpVec<R>>, p: &PermOwned, r: usize) -> Vec<SpVec<R>>
+where R: Ring, for<'x> &'x R: RingOps<R> {
+    vs.into_iter().map(|v| {
+        let n = v.dim();
+        v.permute(p.view()).subvec(r..n).to_owned()
+    }).collect()
+}
+
+fn reduce_tgt_vecs<R>(vs: Vec<SpVec<R>>, p: &PermOwned, s: &SchurLT<R>) -> Vec<SpVec<R>>
+where R: Ring, for<'x> &'x R: RingOps<R> {
+    vs.iter().map(|v| {
+        let v = v.permute(p.view()).to_owned();
+        s.trans_vec(v)
+    }).collect()
+}
+#[cfg(test)]
 mod tests {
     use super::*;
-    use crate::ChainComplex;
     use crate::test::TestChainComplex;
-    use yui_matrix::sparse::SpVec;
 
     #[test]
-    fn s2_2nd() {
+    fn s2() {
         let c = TestChainComplex::<i32>::s2();
-        let d3 = c.d_matrix(3); // zero
-        let d2 = c.d_matrix(2);
-        let d1 = c.d_matrix(1);
 
+        let mut red = ChainReducer::new(&c);
+        red.process();
+
+        let d2 = red.matrix(2).unwrap();
+        let d1 = red.matrix(1).unwrap();
+
+        assert!( (d1 * d2).is_zero() );
+    }
+
+    #[test]
+    fn t2() {
+        let c = TestChainComplex::<i32>::t2();
+
+        let mut red = ChainReducer::new(&c);
+        red.process();
+
+        let d2 = red.matrix(2).unwrap();
+        let d1 = red.matrix(1).unwrap();
+
+        assert!( (d1 * d2).is_zero() );
+    }
+
+    #[test]
+    fn rp2() {
+        let c = TestChainComplex::<i32>::rp2();
+
+        let mut red = ChainReducer::new(&c);
+        red.process();
+
+        let d2 = red.matrix(2).unwrap();
+        let d1 = red.matrix(1).unwrap();
+
+        assert!( (d1 * d2).is_zero() );
+    }
+
+    #[test]
+    fn s2_top() {
+        let c = TestChainComplex::<i32>::s2();
         let v = SpVec::from(vec![-1, 1, -1, 1]);
 
-        let (d3, d2, d1, v_red, _) = ChainReducer::reduce_with(d3, d2, d1, vec![v], vec![]);
+        let mut red = ChainReducer::new(&c);
+        red.set_vec(2, v);
+        red.process();
 
-        assert_eq!((&d2 * &d3).is_zero(), true);
-        assert_eq!((&d1 * &d2).is_zero(), true);
+        let d2 = red.matrix(2).unwrap();
+        let vs = red.vecs(2).unwrap();
 
-        assert_eq!(v_red.len(), 1);
-        assert_eq!(v_red[0].is_zero(), false);
-        assert_eq!((&d2 * &v_red[0]).is_zero(), true);
+        assert_eq!(vs.len(), 1);
+
+        let v = &vs[0];
+
+        assert_eq!(v.is_zero(), false);
+        assert!( (d2 * v).is_zero() );
     }
 
     #[test]
-    fn t2_2nd() {
+    fn t2_top() {
         let c = TestChainComplex::<i32>::t2();
-        let d3 = c.d_matrix(3); // zero
-        let d2 = c.d_matrix(2);
-        let d1 = c.d_matrix(1);
-
         let v = SpVec::from(vec![1,1,-1,1,-1,-1,1,-1,1,-1,-1,1,1,-1,1,-1,-1,1]);
 
-        let (d3, d2, d1, v_red, _) = ChainReducer::reduce_with(d3, d2, d1, vec![v], vec![]);
+        let mut red = ChainReducer::new(&c);
+        red.set_vec(2, v);
+        red.process();
 
-        assert_eq!((&d2 * &d3).is_zero(), true);
-        assert_eq!((&d1 * &d2).is_zero(), true);
-        
-        assert_eq!(v_red.len(), 1);
-        assert_eq!(v_red[0].is_zero(), false);
-        assert_eq!((&d2 * &v_red[0]).is_zero(), true);
+        let d2 = red.matrix(2).unwrap();
+        let vs = red.vecs(2).unwrap();
+
+        assert_eq!(vs.len(), 1);
+
+        let v = &vs[0];
+
+        assert_eq!(v.is_zero(), false);
+        assert!( (d2 * v).is_zero() );
     }
 
     #[test]
-    fn t2_1st_right() {
+    fn t2_1st() {
         let c = TestChainComplex::<i32>::t2();
-        let d3 = c.d_matrix(3);
-        let d2 = c.d_matrix(2);
-        let d1 = c.d_matrix(1);
+        let v0 = SpVec::from(vec![0,0,0,0,0,0,0,0,0,1,-1,0,0,0,0,0,0,0,0,-1,0,0,1,-1,1,0,0]);
+        let v1 = SpVec::from(vec![0,0,0,0,0,0,0,0,0,-1,1,0,0,0,0,0,0,0,0,1,0,-1,0,0,0,-1,1]);
 
-        let v = SpVec::from(vec![0,0,0,0,0,0,0,0,0,1,-1,0,0,0,0,0,0,0,0,-1,0,0,1,-1,1,0,0]);
-        let w = SpVec::from(vec![0,0,0,0,0,0,0,0,0,-1,1,0,0,0,0,0,0,0,0,1,0,-1,0,0,0,-1,1]);
+        let mut red = ChainReducer::new(&c);
+        red.set_vec(1, v0);
+        red.set_vec(1, v1);
+        red.process();
 
-        let (d3, d2, d1, _, v_red) = ChainReducer::reduce_with(d3, d2, d1, vec![], vec![v, w]);
+        let d1 = red.matrix(1).unwrap();
+        let vs = red.vecs(1).unwrap();
 
-        assert_eq!((&d2 * &d3).is_zero(), true);
-        assert_eq!((&d1 * &d2).is_zero(), true);
-        
-        assert_eq!(v_red.len(), 2);
-        for i in 0..2 { 
-            assert_eq!(v_red[i].is_zero(), false);
-            assert_eq!((&d1 * &v_red[i]).is_zero(), true);
-        }
-    }
+        assert_eq!(vs.len(), 2);
 
-    #[test]
-    fn t2_1st_left() {
-        let c = TestChainComplex::<i32>::t2();
-        let d2 = c.d_matrix(2);
-        let d1 = c.d_matrix(1);
-        let d0 = c.d_matrix(0);
+        let v0 = &vs[0];
+        let v1 = &vs[1];
 
-        let v = SpVec::from(vec![0,0,0,0,0,0,0,0,0,1,-1,0,0,0,0,0,0,0,0,-1,0,0,1,-1,1,0,0]);
-        let w = SpVec::from(vec![0,0,0,0,0,0,0,0,0,-1,1,0,0,0,0,0,0,0,0,1,0,-1,0,0,0,-1,1]);
-
-        let (d2, d1, d0, v_red, _) = ChainReducer::reduce_with(d2, d1, d0, vec![v, w], vec![]);
-
-        assert_eq!((&d1 * &d2).is_zero(), true);
-        assert_eq!((&d0 * &d1).is_zero(), true);
-        
-        assert_eq!(v_red.len(), 2);
-        for i in 0..2 { 
-            assert_eq!(v_red[i].is_zero(), false);
-            assert_eq!((&d1 * &v_red[i]).is_zero(), true);
-        }
-
-        dbg!(v_red);
-    }
-
-    #[test]
-    fn t2_1st_both() {
-        let c = TestChainComplex::<i32>::t2();
-        let d3 = c.d_matrix(3); // zero
-        let d2 = c.d_matrix(2);
-        let d1 = c.d_matrix(1);
-        let d0 = c.d_matrix(0);
-
-        let v = SpVec::from(vec![0,0,0,0,0,0,0,0,0,1,-1,0,0,0,0,0,0,0,0,-1,0,0,1,-1,1,0,0]);
-        let w = SpVec::from(vec![0,0,0,0,0,0,0,0,0,-1,1,0,0,0,0,0,0,0,0,1,0,-1,0,0,0,-1,1]);
-
-        let (d3, d2, d1, _, v_red) = ChainReducer::reduce_with(d3, d2, d1, vec![], vec![v, w]);
-        let (d2, d1, d0, v_red, _) = ChainReducer::reduce_with(d2, d1, d0, v_red, vec![]);
-
-        assert_eq!((&d2 * &d3).is_zero(), true);
-        assert_eq!((&d1 * &d2).is_zero(), true);
-        assert_eq!((&d0 * &d1).is_zero(), true);
-        
-        assert_eq!(v_red.len(), 2);
-
-        for i in 0..2 { 
-            assert_eq!(v_red[i].is_zero(), false);
-            assert_eq!((&d1 * &v_red[i]).is_zero(), true);
-        }
+        assert_eq!(v0.is_zero(), false);
+        assert_eq!(v1.is_zero(), false);
+        assert!( (d1 * v0).is_zero() );
+        assert!( (d1 * v1).is_zero() );
     }
 }

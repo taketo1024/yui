e1700f5b0ef4ce928216f60b26b8fa6a15af7d12
taketo1024 <taketo1024@gmail.com>
2023-03-23 13:06:21 +0900

    add tng.

--- workspace/khovanov/src/tools/fast_kh/mod.rs
+++ workspace/khovanov/src/tools/fast_kh/mod.rs
@@ -1,1 +1,3 @@
-// 
+#![allow(dead_code)] // TODO remove 
+
+mod tng;
---
+++ workspace/khovanov/src/tools/fast_kh/tng.rs
@@ -0,0 +1,128 @@
+use std::fmt::Display;
+use itertools::Itertools;
+use yui_link::Component;
+
+#[derive(Clone)]
+pub struct Tng {
+    comps: Vec<Component> // arc or circle
+}
+
+impl Tng { 
+    pub fn new(comps: Vec<Component>) -> Self { 
+        Self { comps }
+    }
+
+    pub fn empty() -> Self { 
+        Self::new(vec![])
+    }
+
+    pub fn is_empty(&self) -> bool { 
+        self.comps.is_empty()
+    }
+
+    pub fn is_closed(&self) -> bool { 
+        self.comps.iter().all(|a| a.is_empty() || a.is_circle())
+    }
+
+    pub fn ncomps(&self) -> usize { 
+        self.comps.len()
+    }
+
+    pub fn comp(&self, i: usize) -> &Component { 
+        &self.comps[i]
+    }
+
+    pub fn append_arc(&mut self, arc: Component) { 
+        assert!(arc.is_arc());
+
+        let n = self.ncomps();
+        if let Some(i) = self.find_connectable(&arc, n) { 
+            self.comps[i].connect(arc);
+
+            // When both ends of `arc` are connectable:
+            if let Some(j) = self.find_connectable(&self.comps[i], i) { 
+                let arc_j = self.comps.remove(j);
+                self.comps[i].connect(arc_j);
+            }
+        } else { 
+            self.comps.push(arc);
+        }
+    }
+
+    fn find_connectable(&self, arc: &Component, j: usize) -> Option<usize> {
+        let n = self.comps.len();
+
+        (0..n).find(|&i| 
+            i != j && self.comps[i].is_connectable(arc)
+        )
+    }
+
+    pub fn find_loop(&self) -> Option<usize> {
+        self.comps.iter().enumerate().find(|(_, c)| 
+            c.is_circle()
+        ).map(|(i, _)|
+            i
+        )
+    }
+
+    pub fn deloop(&mut self, i: usize) -> Component {
+        assert!(self.comps[i].is_circle());
+        self.comps.remove(i)
+    }
+}
+
+impl Display for Tng {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let arcs = self.comps.iter().map(|a| 
+            a.to_string()
+        ).join(", ");
+        write!(f, "T({})", arcs)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+ 
+    #[test]
+    fn append() { 
+        let mut t = Tng::empty();
+        assert_eq!(t.ncomps(), 0);
+
+        t.append_arc(Component::new(vec![0,1], false)); // [0-1]
+        assert_eq!(t.ncomps(), 1);
+
+        t.append_arc(Component::new(vec![2,3], false)); // [0-1] [2-3]
+        assert_eq!(t.ncomps(), 2);
+
+        t.append_arc(Component::new(vec![1,2], false)); // [0-1-2-3]
+        assert_eq!(t.ncomps(), 1);
+
+        t.append_arc(Component::new(vec![0,3], false)); // [-0-1-2-3-]
+        assert_eq!(t.ncomps(), 1);
+    }
+
+    #[test]
+    fn deloop() { 
+        let mut t = Tng::empty();
+        assert_eq!(t.ncomps(), 0);
+        assert_eq!(t.find_loop(), None);
+
+        t.append_arc(Component::new(vec![0,1], false));
+        assert_eq!(t.ncomps(), 1);
+        assert_eq!(t.find_loop(), None);
+
+        t.append_arc(Component::new(vec![2,3], false));
+        assert_eq!(t.ncomps(), 2);
+        assert_eq!(t.find_loop(), None);
+
+        t.append_arc(Component::new(vec![2,3], false));
+        assert_eq!(t.ncomps(), 2);
+        assert_eq!(t.find_loop(), Some(1));
+
+        t.deloop(1);
+
+        assert_eq!(t.ncomps(), 1);
+        assert_eq!(t.find_loop(), None);
+    }
+}

774576c412c1a6011ff46e1b5e60001eb08d6a03
Taketo Sano <taketo1024@gmail.com>
2023-05-08 16:20:23 +0900

    Deserialize for CType.

--- src/utils/ctype.rs
+++ src/utils/ctype.rs
@@ -1,7 +1,8 @@
 use clap::ValueEnum;
+use serde::Deserialize;
 
 #[allow(non_camel_case_types)]
-#[derive(Clone, ValueEnum, Debug, derive_more::Display)]
+#[derive(Clone, ValueEnum, Debug, derive_more::Display, Deserialize)]
 pub enum CType { 
     Z, Q, F2, F3, Gauss, Eisen, None
 }

37dc39594da3874d78465015a7c5784a1f003597
Taketo Sano <taketo1024@gmail.com>
2023-10-16 11:48:44 +0900

    add usize2, 3.

--- workspace/homology/src/v2/deg.rs
+++ workspace/homology/src/v2/deg.rs
@@ -1,39 +1,58 @@
-use std::ops::{Add, Sub, Neg};
+use std::ops::{Add, Sub};
 use std::hash::Hash;
 
-use derive_more::{Add, Sub, Neg};
+use derive_more::{Add, Sub};
 use num_traits::Zero;
 
-pub trait Deg: Clone + Copy + PartialEq + Eq + Hash + Zero + Add + Sub + Neg {}
+pub trait Deg: Clone + Copy + PartialEq + Eq + Hash + Zero + Add + Sub {}
 
-impl Deg for isize {}
+impl Deg for isize  {}
+impl Deg for usize  {}
+
+macro_rules! make2 {
+    ($name:ident, $t:ty) => {
+        #[allow(non_camel_case_types)]
+        #[derive(Clone, Copy, PartialEq, Eq, Hash, Add, Sub)]
+        pub struct $name(pub $t, pub $t);
+        
+        impl Zero for $name {
+            fn zero() -> Self {
+                Self(0, 0)
+            }
+        
+            fn is_zero(&self) -> bool {
+                self.0.is_zero() && self.1.is_zero()
+            }
+        }
+    };
+}
+
+make2!(isize2, isize);
+make2!(usize2, usize);
+
 impl Deg for isize2 {}
+impl Deg for usize2 {}
+
+macro_rules! make3 {
+    ($name:ident, $t:ty) => {
+        #[allow(non_camel_case_types)]
+        #[derive(Clone, Copy, PartialEq, Eq, Hash, Add, Sub)]
+        pub struct $name(pub $t, pub $t, pub $t);
+        
+        impl Zero for $name {
+            fn zero() -> Self {
+                Self(0, 0, 0)
+            }
+        
+            fn is_zero(&self) -> bool {
+                self.0.is_zero() && self.1.is_zero() && self.2.is_zero()
+            }
+        }
+    };
+}
+
+make3!(isize3, isize);
+make3!(usize3, usize);
+
 impl Deg for isize3 {}
-
-#[allow(non_camel_case_types)]
-#[derive(Clone, Copy, PartialEq, Eq, Hash, Add, Sub, Neg)]
-pub struct isize2(isize, isize);
-
-impl Zero for isize2 {
-    fn zero() -> Self {
-        Self(0, 0)
-    }
-
-    fn is_zero(&self) -> bool {
-        self.0.is_zero() && self.1.is_zero()
-    }
-}
-
-#[allow(non_camel_case_types)]
-#[derive(Clone, Copy, PartialEq, Eq, Hash, Add, Sub, Neg)]
-pub struct isize3(isize, isize, isize);
-
-impl Zero for isize3 {
-    fn zero() -> Self {
-        Self(0, 0, 0)
-    }
-
-    fn is_zero(&self) -> bool {
-        self.0.is_zero() && self.1.is_zero() && self.2.is_zero()
-    }
-}
+impl Deg for usize3 {}

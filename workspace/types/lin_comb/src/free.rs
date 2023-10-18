bc7b9d45d3db569dad897a2be0b715b601deba1e
Taketo Sano <taketo1024@gmail.com>
2023-10-18 17:06:14 +0900

    split files.

---
+++ workspace/types/lin_comb/src/free.rs
@@ -0,0 +1,24 @@
+use std::hash::Hash;
+use yui_core::Elem;
+
+pub trait OrdForDisplay {
+    fn cmp_for_display(&self, other: &Self) -> std::cmp::Ordering;
+}
+
+impl<T> OrdForDisplay for T where T: Ord { 
+    fn cmp_for_display(&self, other: &Self) -> std::cmp::Ordering {
+        std::cmp::Ord::cmp(self, other)
+    }
+}
+
+pub trait SortForDisplay { 
+    fn sort_for_display(&mut self);
+}
+
+impl<T> SortForDisplay for Vec<T> where T: OrdForDisplay { 
+    fn sort_for_display(&mut self) {
+        self.sort_by(T::cmp_for_display)
+    }
+}
+
+pub trait FreeGen: Elem + Hash + OrdForDisplay {}

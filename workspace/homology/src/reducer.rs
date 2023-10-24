2f40ca09790cb4a2b8059d47c709eca27eb68c88
Taketo Sano <taketo1024@gmail.com>
2023-10-24 16:57:26 +0900

    impl update_trans, add tests.

--- workspace/homology/src/reducer.rs
+++ workspace/homology/src/reducer.rs
@@ -31,8 +31,7 @@ where
     complex: &'a ChainComplexBase<I, R>,
     mats: HashMap<I, SpMat<R>>,
     with_trans: bool,
-    trans: Option<HashMap<I, SpMat<R>>>,
-    trans_rev: Option<HashMap<I, SpMat<R>>>,
+    trans: Option<HashMap<I, Trans<R>>>
 }
 
 impl<'a, I, R> ChainReducer<'a, I, R>
@@ -48,12 +47,12 @@ where
 
     pub fn new(complex: &'a ChainComplexBase<I, R>, with_trans: bool) -> Self { 
         let mats = HashMap::new();
-        let (trans, trans_rev) = if with_trans { 
-            (Some(HashMap::new()), Some(HashMap::new()))
+        let trans = if with_trans { 
+            Some(HashMap::new())
         } else { 
-            (None, None)
+            None
         };
-        Self { complex, mats, with_trans, trans, trans_rev }
+        Self { complex, mats, with_trans, trans }
     }
 
     pub fn as_complex(mut self) -> ChainComplexBase<I, R> { 
@@ -72,12 +71,8 @@ where
         self.mats.remove(&i).unwrap_or(self.complex.d_matrix(i).clone())
     }
 
-    pub fn trans(&self, i: I) -> Option<&SpMat<R>> {
+    pub fn trans(&self, i: I) -> Option<&Trans<R>> {
         self.trans.as_ref().and_then(|t| t.get(&i))
-    }
-
-    pub fn trans_rev(&self, i: I) -> Option<&SpMat<R>> {
-        self.trans_rev.as_ref().and_then(|t| t.get(&i))
     }
 
     pub fn process_all(&mut self) { 
@@ -118,7 +113,31 @@ where
     }
 
     fn update_trans(&mut self, i: I, p: &PermOwned, q: &PermOwned, r: usize, s: &Schur<R>) {
-        // TODO
+        assert!(self.with_trans);
+
+        let (m, n)   = self.matrix(i).shape(); // (m, n)
+        let (_, i1, i2) = self.deg_trip(i);
+        
+        if self.trans(i1).is_none() { 
+            self.trans.as_mut().unwrap().insert(i1, Trans::id(n));
+        }
+        if self.trans(i2).is_none() { 
+            self.trans.as_mut().unwrap().insert(i2, Trans::id(m));
+        }
+
+        let t1 = self.trans(i1).unwrap().compose_perm(q.view()).map(
+            |a| a.submat_rows(r..n),
+            |b| b * s.trans_in().unwrap()
+        );
+
+        let t2 = self.trans(i2).unwrap().compose_perm(p.view()).map(
+            |a| s.trans_out().unwrap() * a,
+            |b| b.submat_cols(r..m)
+        );
+
+        let ts = self.trans.as_mut().unwrap();
+        ts.insert(i1, t1);
+        ts.insert(i2, t2);
     }
 
     fn update_mats(&mut self, i: I, p: &PermOwned, q: &PermOwned, r: usize, s: Schur<R>) {
@@ -209,5 +228,110 @@ mod tests {
         let d1 = red.matrix(1);
 
         assert!( (d1 * d2).is_zero() );
+    }
+
+    #[test]
+    fn s2_trans() {
+        let c = Samples::<i32>::s2();
+
+        let mut red = ChainReducer::new(&c, true);
+        red.process_all();
+
+        let t0 = red.trans(0).unwrap();
+        let t1 = red.trans(1).unwrap();
+        let t2 = red.trans(2).unwrap();
+
+        assert_eq!(t0.src_dim(), 4);
+        assert_eq!(t1.src_dim(), 6);
+        assert_eq!(t2.src_dim(), 4);
+
+        assert_eq!(t0.tgt_dim(), 1);
+        assert_eq!(t1.tgt_dim(), 0);
+        assert_eq!(t2.tgt_dim(), 1);
+
+        assert_eq!(t0.f_mat() * t0.b_mat(), SpMat::id(1));
+        assert_eq!(t1.f_mat() * t1.b_mat(), SpMat::id(0));
+        assert_eq!(t2.f_mat() * t2.b_mat(), SpMat::id(1));
+
+        let v = SpVec::unit(1, 0);
+        let w = t2.trans_b(&v);
+        
+        assert!(c.is_cycle(2, &w));
+    }
+
+    #[test]
+    fn t2_trans() {
+        let c = Samples::<i32>::t2();
+
+        let mut red = ChainReducer::new(&c, true);
+        red.process_all();
+
+        let t0 = red.trans(0).unwrap();
+        let t1 = red.trans(1).unwrap();
+        let t2 = red.trans(2).unwrap();
+
+        assert_eq!(t0.src_dim(), 9);
+        assert_eq!(t1.src_dim(), 27);
+        assert_eq!(t2.src_dim(), 18);
+
+        assert_eq!(t0.tgt_dim(), 1);
+        assert_eq!(t1.tgt_dim(), 2);
+        assert_eq!(t2.tgt_dim(), 1);
+
+        assert_eq!(t0.f_mat() * t0.b_mat(), SpMat::id(1));
+        assert_eq!(t1.f_mat() * t1.b_mat(), SpMat::id(2));
+        assert_eq!(t2.f_mat() * t2.b_mat(), SpMat::id(1));
+
+        let v = SpVec::unit(1, 0);
+        let w = t2.trans_b(&v);
+        
+        assert!(c.is_cycle(2, &w));
+
+        let a = SpVec::unit(2, 0);
+        let b = SpVec::unit(2, 1);
+        let a = t1.trans_b(&a);
+        let b = t1.trans_b(&b);
+        
+        assert!(c.is_cycle(1, &a));
+        assert!(c.is_cycle(1, &b));
+    }
+
+    #[test]
+    fn rp2_trans() {
+        let c = Samples::<i32>::rp2();
+
+        let mut red = ChainReducer::new(&c, true);
+        red.process_all();
+
+        let t0 = red.trans(0).unwrap();
+        let t1 = red.trans(1).unwrap();
+        let t2 = red.trans(2).unwrap();
+
+        assert_eq!(t0.src_dim(), 6);
+        assert_eq!(t1.src_dim(), 15);
+        assert_eq!(t2.src_dim(), 10);
+
+        assert_eq!(t0.tgt_dim(), 1);
+        assert_eq!(t1.tgt_dim(), 1);
+        assert_eq!(t2.tgt_dim(), 1);
+
+        assert_eq!(t0.f_mat() * t0.b_mat(), SpMat::id(1));
+        assert_eq!(t1.f_mat() * t1.b_mat(), SpMat::id(1));
+        assert_eq!(t2.f_mat() * t2.b_mat(), SpMat::id(1));
+
+        let v = SpVec::unit(1, 0); // generates 2
+        let w = t2.trans_b(&v);
+        let dw = c.differentiate(2, &w);
+        let dv = t1.trans_f(&dw);
+
+        assert!(
+            dv == SpVec::from(vec![ 2]) || 
+            dv == SpVec::from(vec![-2])
+        );
+
+        let v = SpVec::unit(1, 0);
+        let w = t1.trans_b(&v);
+        
+        assert!(c.is_cycle(1, &w));
     }
 }

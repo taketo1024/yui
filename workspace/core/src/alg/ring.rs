f3a6b55dcda9b849e6036f6b7a01e0f7266e0c34
Taketo Sano <taketo1024@gmail.com>
2023-11-06 17:00:55 +0900

    is_pm_one.

--- workspace/core/src/alg/ring.rs
+++ workspace/core/src/alg/ring.rs
@@ -18,7 +18,24 @@ where
     fn inv(&self) -> Option<Self>;
     fn is_unit(&self) -> bool;
     fn normalizing_unit(&self) -> Self;
+
+    fn is_pm_one(&self) -> bool { 
+        self.is_one() || (-self).is_one()
+    }
     fn from_sign(s: Sign) -> Self { 
         Self::from(s.to_i32())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::Ring;
+ 
+    #[test]
+    fn is_pm_one() { 
+        assert!(1.is_pm_one());
+        assert!((-1).is_pm_one());
+        assert!(!2.is_pm_one());
+        assert!(!(-2).is_pm_one());
     }
 }

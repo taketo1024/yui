f47c45ba2fcca063e3e990e5b8f76c6c8794f606
Taketo Sano <taketo1024@gmail.com>
2023-11-29 10:25:45 +0900

    drop Product from Mon.

--- yui/src/abst/mon.rs
+++ yui/src/abst/mon.rs
@@ -1,4 +1,3 @@
-use std::iter::Product;
 use std::ops::{Mul, MulAssign};
 use num_traits::One;
 use crate::Elem;
@@ -13,12 +12,31 @@ pub trait MonOps<T = Self>:
 
 pub trait Mon: 
     Elem + 
+    One +
     MonOps + 
     MulAssign + 
-    for<'a> MulAssign<&'a Self> + 
-    Product<Self> + 
-    for<'a> Product<&'a Self> +
-    One
+    for<'a> MulAssign<&'a Self>
 where
     for<'a> &'a Self: MonOps<Self>
-{}
+{
+    fn product<A, I>(itr: I) -> Self 
+    where 
+        Self: MulAssign<A>,
+        I: IntoIterator<Item = A> 
+    { 
+        itr.into_iter().fold(Self::one(), |mut res, a| { 
+            res *= a;
+            res
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests { 
+    use super::*;
+    #[test]
+    fn product() { 
+        let a = i64::product([4,5,6]);
+        assert_eq!(a, 120);
+    }
+}

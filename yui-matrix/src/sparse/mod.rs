0c5f62ba03653a7304cecf6c09ea1e7bc2c31ed9
Taketo Sano <taketo1024@gmail.com>
2023-11-30 14:41:24 +0900

    replace sp_mat/vec with _sp_mat/vec.

--- yui-matrix/src/sparse/mod.rs
+++ yui-matrix/src/sparse/mod.rs
@@ -1,15 +1,20 @@
-mod sp_mat;
-mod sp_vec;
-mod trans;
-
-pub use sp_mat::{SpMat, SpMatView};
-pub use sp_vec::{SpVec, SpVecView};
-pub use trans::*;
 pub use crate::dense::MatType;
+
+// mod sp_mat;
+// mod sp_vec;
+// pub use sp_mat::{SpMat, SpMatView};
+// pub use sp_vec::{SpVec, SpVecView};
+
+mod _sp_mat;
+mod _sp_vec;
+pub use _sp_mat::{SpMat, SpMatView};
+pub use _sp_vec::{SpVec, SpVecView};
+
+
+mod trans;
+pub use trans::*;
 
 pub mod pivot;
 pub mod schur;
 pub mod triang;
 
-mod _sp_mat;
-mod _sp_vec;

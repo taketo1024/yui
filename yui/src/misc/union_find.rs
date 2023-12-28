60eb192a9544d842907fc35c7a8d2f129ce5b1ef
Taketo Sano <taketo1024@gmail.com>
2023-12-28 10:47:13 +0900

    add test.

--- yui/src/misc/union_find.rs
+++ yui/src/misc/union_find.rs
@@ -10,6 +10,10 @@ impl UnionFind {
         Self { roots }
     }
 
+    pub fn size(&self) -> usize { 
+        self.roots.len()
+    }
+
     pub fn is_same(&self, i: usize, j: usize) -> bool { 
         self.roots[i] == self.roots[j]
     }
@@ -26,7 +30,37 @@ impl UnionFind {
     }
 
     pub fn group(&self) -> Vec<Vec<usize>> { 
-        let n = self.roots.len();
+        let n = self.size();
         (0..n).into_group_map_by(|&i| self.roots[i]).into_values().collect()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::UnionFind;
+ 
+    #[test]
+    fn test() { 
+        let mut u = UnionFind::new(4);
+        
+        assert_eq!(u.size(), 4);
+        assert!(!u.is_same(0, 1));
+        assert!(!u.is_same(0, 2));
+        assert!(!u.is_same(1, 2));
+        assert!(!u.is_same(0, 3));
+
+        u.union(0, 1);
+
+        assert!( u.is_same(0, 1));
+        assert!(!u.is_same(0, 2));
+        assert!(!u.is_same(1, 2));
+        assert!(!u.is_same(0, 3));
+
+        u.union(1, 2);
+
+        assert!( u.is_same(0, 1));
+        assert!( u.is_same(0, 2));
+        assert!( u.is_same(1, 2));
+        assert!(!u.is_same(0, 3));
     }
 }

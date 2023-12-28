49e9147f900daa748fff3d5cfaa986fb71081dbb
Taketo Sano <taketo1024@gmail.com>
2023-12-28 13:55:13 +0900

    fix UnionFind.

--- yui/src/misc/union_find.rs
+++ yui/src/misc/union_find.rs
@@ -1,42 +1,53 @@
 use itertools::Itertools;
 
 pub struct UnionFind { 
-    roots: Vec<usize>
+    p: Vec<usize>
 }
 
 impl UnionFind { 
     pub fn new(n: usize) -> Self { 
-        let roots = (0..n).collect();
-        Self { roots }
+        Self { p: (0..n).collect() }
     }
 
     pub fn size(&self) -> usize { 
-        self.roots.len()
+        self.p.len()
     }
 
+    pub fn root(&self, i: usize) -> usize { 
+        let p = self.p[i];
+        if p == i { 
+            i
+        } else { 
+            self.root(p)
+        }
+    }
+    
     pub fn is_same(&self, i: usize, j: usize) -> bool { 
-        self.roots[i] == self.roots[j]
+        self.root(i) == self.root(j)
     }
 
     pub fn union(&mut self, i: usize, j: usize) {
-        let ri = self.roots[i];
-        let rj = self.roots[j];
+        use std::cmp::Ordering::*;
+        let ri = self.root(i);
+        let rj = self.root(j);
 
         match usize::cmp(&ri, &rj) {
-            std::cmp::Ordering::Less    => self.roots[j] = ri,
-            std::cmp::Ordering::Equal   => (),
-            std::cmp::Ordering::Greater => self.roots[i] = rj,
+            Less    => self.p[rj] = ri,
+            Equal   => (),
+            Greater => self.p[ri] = rj,
         }
     }
 
     pub fn group(&self) -> Vec<Vec<usize>> { 
         let n = self.size();
-        (0..n).into_group_map_by(|&i| self.roots[i]).into_values().collect()
+        (0..n).into_group_map_by(|&i| self.root(i)).into_iter().sorted_by_key(|&(i, _)| i).map(|(_, l)| l).collect()
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use itertools::Itertools;
+
     use crate::UnionFind;
  
     #[test]
@@ -44,23 +55,39 @@ mod tests {
         let mut u = UnionFind::new(4);
         
         assert_eq!(u.size(), 4);
+        assert_eq!(&u.p, &vec![0,1,2,3]);
+        assert_eq!((0..4).map(|i| u.root(i)).collect_vec(), vec![0,1,2,3]);
+
         assert!(!u.is_same(0, 1));
-        assert!(!u.is_same(0, 2));
         assert!(!u.is_same(1, 2));
-        assert!(!u.is_same(0, 3));
+        assert!(!u.is_same(2, 3));
 
         u.union(0, 1);
 
+        assert_eq!(&u.p, &vec![0,0,2,3]);
+        assert_eq!((0..4).map(|i| u.root(i)).collect_vec(), vec![0,0,2,3]);
+
         assert!( u.is_same(0, 1));
-        assert!(!u.is_same(0, 2));
         assert!(!u.is_same(1, 2));
-        assert!(!u.is_same(0, 3));
+        assert!(!u.is_same(2, 3));
 
-        u.union(1, 2);
+        u.union(2, 3);
+
+        assert_eq!(&u.p, &vec![0,0,2,2]);
+        assert_eq!((0..4).map(|i| u.root(i)).collect_vec(), vec![0,0,2,2]);
 
         assert!( u.is_same(0, 1));
-        assert!( u.is_same(0, 2));
+        assert!(!u.is_same(1, 2));
+        assert!( u.is_same(2, 3));
+
+        u.union(1, 3);
+
+        assert_eq!(&u.p, &vec![0,0,0,2]);
+        assert_eq!((0..4).map(|i| u.root(i)).collect_vec(), vec![0,0,0,0]);
+
+        assert!( u.is_same(0, 1));
         assert!( u.is_same(1, 2));
-        assert!(!u.is_same(0, 3));
+        assert!( u.is_same(2, 3));
+
     }
 }

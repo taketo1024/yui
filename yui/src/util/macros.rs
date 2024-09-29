c22890d3a6bfdf2254aecfc65897618c4621533e
Taketo Sano <taketo1024@gmail.com>
2024-09-29 22:53:22 +0900

    improve hashmap!

--- yui/src/util/macros.rs
+++ yui/src/util/macros.rs
@@ -4,14 +4,14 @@
 #[macro_export]
 macro_rules! hashmap {
     {$( $key: expr => $val: expr ),*} => {{
-        std::collections::HashMap::from_iter([$(($key, $val),)*])
+        std::collections::HashMap::<_, _, std::hash::RandomState>::from_iter([$(($key, $val),)*])
     }}
 }
 
 #[macro_export]
 macro_rules! hashset {
     {$($val: expr ),*} => {{
-        std::collections::HashSet::from_iter([$($val,)*])
+        std::collections::HashSet::<_, std::hash::RandomState>from_iter([$($val,)*])
     }}
 }
 

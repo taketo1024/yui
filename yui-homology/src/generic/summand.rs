983cdacf334b63c3e35baba9638409afe8152618
Taketo Sano <taketo1024@gmail.com>
2024-10-11 17:50:45 +0900

    GenericSummand::generate

--- yui-homology/src/generic/summand.rs
+++ yui-homology/src/generic/summand.rs
@@ -1,4 +1,5 @@
 use yui::{Ring, RingOps};
+use yui_matrix::sparse::Trans;
 
 use crate::{GridDeg, XModStr};
 
@@ -8,7 +9,17 @@ pub type GenericSummand<I, R> = XModStr<EnumGen<I>, R>;
 
 impl<I, R> GenericSummand<I, R>
 where I: GridDeg, R: Ring, for<'x> &'x R: RingOps<R> { 
+    pub fn generate(i: I, rank: usize, tors: Vec<R>, trans: Option<Trans<R>>) -> Self { 
+        let n = if let Some(t) = &trans { 
+            t.src_dim()
+        } else { 
+            rank + tors.len()
+        };
+        let gens = (0..n).map(|j| EnumGen(i, j)).collect();
+        Self::new(gens, rank, tors, trans)
+    }
+
     pub fn generate_free(i: I, rank: usize) -> Self { 
-        Self::free((0..rank).map(|j| EnumGen(i, j)))
+        Self::generate(i, rank, vec![], Some(Trans::id(rank)))
     }
 }

7bcc0f410de3e0d3e1d122f32c50ff9868ef6978
Taketo Sano <taketo1024@gmail.com>
2024-10-11 18:02:44 +0900

    compute_homology

--- yui-homology/src/cpx/homology.rs
+++ yui-homology/src/cpx/homology.rs
@@ -1,5 +1,6 @@
-use yui::{EucRing, EucRingOps};
+use yui::{EucRing, EucRingOps, Ring, RingOps};
 
+use crate::generic::GenericSummand;
 use crate::utils::HomologyCalc;
 use crate::{GridDeg, Grid, GridTrait, isize2, isize3};
 use super::{ChainComplexTrait, SimpleRModStr, ChainComplexBase};
@@ -11,34 +12,35 @@ pub type Homology<R>  = HomologyBase<isize,  R>;
 pub type Homology2<R> = HomologyBase<isize2, R>;
 pub type Homology3<R> = HomologyBase<isize3, R>;
 
-pub trait ComputeHomology<I> {
-    type Output;
-    fn compute_homology(&self, i: I, with_trans: bool) -> Self::Output;
+pub trait ComputeHomology<I, R>
+where I: GridDeg, R: Ring, for<'x> &'x R: RingOps<R> {
+    fn compute_homology_at(&self, i: I, with_trans: bool) -> GenericSummand<I, R>;
+    fn compute_homology(&self, with_trans: bool) -> Grid<I, GenericSummand<I, R>>;
 }
 
-impl<I, R, C> ComputeHomology<I> for C 
+impl<I, R, C> ComputeHomology<I, R> for C 
 where 
     I: GridDeg, 
     R: EucRing, for<'x> &'x R: EucRingOps<R>,
     C: ChainComplexTrait<I, R = R>
 {
-    type Output = HomologySummand<R>;
-
-    fn compute_homology(&self, i: I, with_trans: bool) -> HomologySummand<R> {
+    fn compute_homology_at(&self, i: I, with_trans: bool) -> GenericSummand<I, R> {
         let i0 = i - self.d_deg();
         let d0 = self.d_matrix(i0);
-        let d1 = self.d_matrix(i );
-        HomologyCalc::calculate(d0, d1, with_trans)
+        let d1 = self.d_matrix(i);
+        let (rank, tors, trans) = HomologyCalc::calculate(d0, d1, with_trans);
+        GenericSummand::generate(i, rank, tors, trans)
+    }
+
+    fn compute_homology(&self, with_trans: bool) -> Grid<I, GenericSummand<I, R>> {
+        Grid::generate(self.support(), |i| self.compute_homology_at(i, with_trans))
     }
 }
 
 impl<I, R> ChainComplexBase<I, R> 
 where I: GridDeg, R: EucRing, for<'x> &'x R: EucRingOps<R> {
-    pub fn homology_at(&self, i: I, with_trans: bool) -> HomologySummand<R> {
-        let mut c = self[i].clone();
-        let h = self.compute_homology(i, with_trans);
-        c.merge(h, true);
-        c
+    pub fn homology_at(&self, _i: I, _with_trans: bool) -> HomologySummand<R> {
+        todo!("to be removed")
     }
 
     pub fn homology(&self, with_trans: bool) -> HomologyBase<I, R> {

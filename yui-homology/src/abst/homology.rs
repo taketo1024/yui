03de15b08d70eadfc948f877db18c31396fe19bf
Taketo Sano <taketo1024@gmail.com>
2024-10-11 22:32:55 +0900

    GenericHomology

--- yui-homology/src/abst/homology.rs
+++ yui-homology/src/abst/homology.rs
@@ -2,13 +2,13 @@ use yui::{EucRing, EucRingOps, Ring, RingOps};
 
 use crate::generic::GenericSummand;
 use crate::utils::HomologyCalc;
-use crate::{GridDeg, Grid};
+use crate::{GenericHomologyBase, Grid, GridDeg};
 use super::ChainComplexTrait;
 
 pub trait ComputeHomology<I, R>
 where I: GridDeg, R: Ring, for<'x> &'x R: RingOps<R> {
     fn compute_homology_at(&self, i: I, with_trans: bool) -> GenericSummand<I, R>;
-    fn compute_homology(&self, with_trans: bool) -> Grid<I, GenericSummand<I, R>>;
+    fn compute_homology(&self, with_trans: bool) -> GenericHomologyBase<I, R>;
 }
 
 impl<I, R, C> ComputeHomology<I, R> for C 
@@ -25,7 +25,7 @@ where
         GenericSummand::generate(i, rank, tors, trans)
     }
 
-    fn compute_homology(&self, with_trans: bool) -> Grid<I, GenericSummand<I, R>> {
+    fn compute_homology(&self, with_trans: bool) -> GenericHomologyBase<I, R> {
         Grid::generate(self.support(), |i| self.compute_homology_at(i, with_trans))
     }
 }

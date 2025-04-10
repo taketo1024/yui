f8bc12478cd44d92ba0e2e34d233d8417ecf822f
Taketo Sano <taketo1024@gmail.com>
2025-04-10 14:51:38 +0900

    merge free.rs into gen.rs.

--- yui/src/types/lc/gen.rs
+++ yui/src/types/lc/gen.rs
@@ -2,9 +2,34 @@ use std::hash::Hash;
 use derive_more::Display;
 use itertools::Either;
 
-use crate::Elem;
+use crate::{Elem, ElemBase};
 
 pub trait Gen: Elem + Hash + Ord {}
+
+#[derive(Debug, Display, Default, Hash, PartialEq, Eq, Clone, PartialOrd, Ord)]
+#[display("<{}>", _0)]
+#[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
+#[cfg_attr(feature = "serde", serde(transparent))]
+pub struct FreeGen<T>(pub T) where T: ElemBase;
+
+impl<T> From<T> for FreeGen<T> 
+where T: ElemBase {
+    fn from(value: T) -> Self {
+        Self(value)
+    }
+}
+
+impl<T> Elem for FreeGen<T> 
+where T: ElemBase { 
+    fn math_symbol() -> String {
+        let full_name = std::any::type_name::<T>();
+        let name = full_name.split("::").last().unwrap_or(full_name);
+        format!("Free<{}>", name)
+    }
+}
+
+impl<T> Gen for FreeGen<T> 
+where T: ElemBase + Hash + Ord {}
 
 #[derive(Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord, Debug, Display)]
 pub struct EitherGen<X, Y>(Either<X, Y>) where X: Gen, Y: Gen;

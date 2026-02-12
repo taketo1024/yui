deb9faf22bdaf87fc97deb1dddf84ed666d14197
Taketo Sano <taketo1024@gmail.com>
2026-02-12 09:49:44 +0900

    fix fnf, add tests.

--- lib-matrix/src/dense/snf.rs
+++ lib-matrix/src/dense/snf.rs
@@ -35,16 +35,13 @@ pub fn fnf<R>(a: &Mat<R>, flags: SnfFlags) -> SnfResult<Poly<'x', R>>
 where R: Field, for<'x> &'x R: FieldOps<R> { 
     assert!(a.is_square());
 
-    let data = a.iter().map(|(i, j, r)| {
-        let r = Poly::from_const(r.clone());
-        if i == j { 
-            Poly::variable() - r
-        } else { 
-            -r
-        }
-    });
-    let target = Mat::from_data(a.shape(), data);
-    snf_in_place::<Poly<'x', R>>(target, flags)
+    type P<R> = Poly<'x', R>;
+
+    let x = Mat::scalar(a.nrows(), &P::variable());
+    let a = a.map(|r| P::from_const(r.clone()) );
+    let target = x - a;
+    
+    snf_in_place::<P<R>>(target, flags)
 }
 
 #[derive(Debug)]
@@ -503,6 +500,8 @@ use {preprocess_lll_for, preprocess_lll_expand};
 
 #[cfg(test)]
 mod tests {
+    use yui_core::num::Ratio;
+
     use super::*;
 
     #[test]
@@ -921,5 +920,37 @@ mod tests {
 
         assert_eq!(p * a.clone(), res);
         assert_eq!(pinv * res, a.clone());
+    }
+
+    #[test]
+    fn test_snf() { 
+        let a = Mat::from_data((3, 3), [1,2,3,4,5,6,7,8,9]);
+        let (res, trans) = snf(&a, [true; 4]).destruct();
+        let [p, pinv, q, qinv] = trans.map( |p| p.unwrap() );
+
+        assert_eq!(&p * &a * &q, res);
+        assert_eq!(&pinv * &res * &qinv, a);
+        assert!(res.is_diag());
+
+        assert!(res[(0, 0)].divides(&res[(1, 1)]));
+        assert!(res[(1, 1)].divides(&res[(2, 2)]));
+    }
+
+    #[test]
+    fn test_fnf() { 
+        type R = Ratio<i64>;
+        type P = Poly<'x', R>;
+
+        let a = Mat::from_data((3, 3), [1,2,3,4,5,6,7,8,9]).map(|r| R::from(*r));
+        let (res, trans) = fnf(&a, [true; 4]).destruct();
+        let [p, pinv, q, qinv] = trans.map( |p| p.unwrap() );
+
+        let x = Mat::scalar(3, &P::variable()) - a.map(|r| P::from_const(r.clone()));
+
+        assert_eq!(&p * &x * &q, res);
+        assert_eq!(&pinv * &res * &qinv, x);
+
+        assert!(res[(0, 0)].divides(&res[(1, 1)]));
+        assert!(res[(1, 1)].divides(&res[(2, 2)]));
     }
 }

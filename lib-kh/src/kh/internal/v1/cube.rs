bd8f95886cc9c7a8c880aa8dc0355766937bc206
Taketo Sano <taketo1024@gmail.com>
2026-05-15 22:53:57 +0900

    rename Link methods.

--- lib-kh/src/kh/internal/v1/cube.rs
+++ lib-kh/src/kh/internal/v1/cube.rs
@@ -17,7 +17,7 @@ pub struct KhCubeVertex {
 
 impl KhCubeVertex { 
     pub fn new(l: &Link, state: State, red_e: Option<Edge>, deg_shift: (isize, isize)) -> Self {
-        let mut circles = l.resolved_by(&state).collect_components();
+        let mut circles = l.resolved_by(&state).comps();
         circles.sort_by_key(|c| c.min_edge());
         
         let r = circles.len();
@@ -123,7 +123,7 @@ where R: Ring, for<'x> &'x R: RingOps<R> {
     pub fn new(l: &Link, h: &R, t: &R, reduce_e: Option<Edge>, deg_shift: (isize, isize)) -> Self { 
         assert!(reduce_e.is_none() || t.is_zero());
 
-        let n = l.count_crossings();
+        let n = l.n_crossings();
         let str = KhAlg::new(h, t);
 
         let vertices: HashMap<_, _> = State::generate(n).map(|s| { 

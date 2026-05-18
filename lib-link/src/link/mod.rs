2e9088e0b17cfca47ecfe151e6cdf77618bd3552
Taketo Sano <taketo1024@gmail.com>
2026-05-18 17:04:23 +0900

    relocate files.

--- lib-link/src/link/mod.rs
+++ lib-link/src/link/mod.rs
@@ -1,7 +1,11 @@
 mod link;
 mod node;
 mod path;
+mod graph;
+mod inv_link;
 
 pub use link::{Link, Edge, State, XCode};
 pub use node::{Node, NodeType, NodeOri};
 pub use path::Path;
+pub use graph::seifert_graph;
+pub use inv_link::InvLink;

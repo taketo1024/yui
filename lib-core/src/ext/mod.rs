b9cdf9558af40db73b216b486e40ca1020449893
Taketo Sano <taketo1024@gmail.com>
2026-05-20 11:53:32 +0900

    remove PowMod2.

--- lib-core/src/ext/mod.rs
+++ lib-core/src/ext/mod.rs
@@ -2,14 +2,12 @@ mod clone_and;
 mod digits;
 mod div_round;
 mod int;
-mod pow_mod2;
 mod range;
 
 pub use clone_and::*;
 pub use digits::*;
 pub use div_round::*;
 pub use int::*;
-pub use pow_mod2::*;
 pub use range::*;
 
 cfg_if::cfg_if! {

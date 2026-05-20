4233b9fec1c929379d37de5bc7c550397b260c04
Taketo Sano <taketo1024@gmail.com>
2026-05-20 12:39:26 +0900

    add IterExt tests.

--- lib-core/src/ext/iter.rs
+++ lib-core/src/ext/iter.rs
@@ -16,3 +16,34 @@ pub trait IteratorExt: Iterator + Sized {
 }
 
 impl<I: Iterator> IteratorExt for I {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn range_empty() {
+        let r: Option<RangeInclusive<i32>> = std::iter::empty().range();
+        assert_eq!(r, None);
+    }
+
+    #[test]
+    fn range_single() {
+        assert_eq!([7].into_iter().range(), Some(7 ..= 7));
+    }
+
+    #[test]
+    fn range_sorted() {
+        assert_eq!([1, 2, 3, 4, 5].into_iter().range(), Some(1 ..= 5));
+    }
+
+    #[test]
+    fn range_unsorted() {
+        assert_eq!([3, -2, 5, 1, -7, 4].into_iter().range(), Some(-7 ..= 5));
+    }
+
+    #[test]
+    fn range_duplicates() {
+        assert_eq!([2, 2, 2].into_iter().range(), Some(2 ..= 2));
+    }
+}

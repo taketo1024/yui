9081890624e2be5b3f656ed6f75b1cbeb18206e4
Taketo Sano <taketo1024@gmail.com>
2026-08-07 14:06:26 +0900

    lib-homology: restore GrMod::total_rank

--- lib-homology/src/conc/gr_mod.rs
+++ lib-homology/src/conc/gr_mod.rs
@@ -63,6 +63,10 @@ where
         }
     }
 
+    pub fn total_rank(&self) -> usize {
+        self.support().map(|&i| self[i].rank()).sum()
+    }
+
     // Relabel generators by an injective `f`, preserving the grading.
     pub fn map_keys<Y, F>(&self, f: F) -> GrMod<I, Y, R>
     where Y: LcKey, F: Fn(&X) -> Y {
